c96e7edc47eec9698ec87868f2211eb7c37f1dbc
creator-lokpadi <72179525+creator-lokpadi@users.noreply.github.com>
2022-07-17 08:43:21 +0800

    Add files via upload

---
+++ stream.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+while true
+do
+  ffmpeg -loglevel info -y -re \
+    -f image2 -loop 1 -i bg.png \
+    -f concat -safe 0 -i <((for f in ./mp3/*.mp3; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
+    -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
+    -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
+    -c:a aac -b:a 128k -ar 44100 \
+    -f flv rtmp://a.rtmp.youtube.com/live2/xac2-betx-98kw-zy19-5zqb
+done
